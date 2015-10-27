5f49981bee9a3ba15dca18d08184e1b38f949b4b
umanoidTyphoon <umanoidTyphoon@tiscali.com>
2015-10-27 18:00:01 +0100

    Setting up environment to carry out model analysis

--- demo/model_analysis/run_model_analysis.py
+++ demo/model_analysis/run_model_analysis.py
@@ -1,24 +1,36 @@
 __author__ = 'umanoidTyphoon'
 from build_weka_database import build
 from classifier_model_analyzer import classify_dump
+from weka.classifiers import Classifier
 
 import util
+import weka.core.jvm as jvm
+import weka.core.serialization as serialization
 
 # WINDOW_WIDTH = 10000
 WINDOW_WIDTH = 25
 
-build(WINDOW_WIDTH)
+jvm.start()
 
-conn = util.connect_to_db()
+deserialized_objects = serialization.read_all("default.model")
+classifier = Classifier(jobject=deserialized_objects[0])
 
-cursor = conn.cursor()
+print classifier
 
-cursor.execute("""SELECT max(dump_id) from pe_dumps""")
-lastID = cursor.fetchone()[0]
+jvm.stop()
 
-currID = lastID - WINDOW_WIDTH
-
-while currID < lastID:
-    classify_dump(currID)
-    currID += 1
+# build(WINDOW_WIDTH)
+#
+# conn = util.connect_to_db()
+#
+# cursor = conn.cursor()
+#
+# cursor.execute("""SELECT max(dump_id) from pe_dumps""")
+# lastID = cursor.fetchone()[0]
+#
+# currID = lastID - WINDOW_WIDTH
+#
+# while currID < lastID:
+#     classify_dump(currID)
+#     currID += 1
 
