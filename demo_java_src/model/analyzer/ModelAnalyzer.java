5b017f3123b607a020c88b998bf09df3d5d64caf
umanoidTyphoon <umanoidTyphoon@tiscali.com>
2015-11-06 19:23:34 +0100

    Implemented the 'writeFormattedStats()' method

--- demo_java_src/model/analyzer/ModelAnalyzer.java
+++ demo_java_src/model/analyzer/ModelAnalyzer.java
@@ -12,7 +12,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
@@ -32,11 +31,7 @@ public class ModelAnalyzer {
 	private static final int  PARENT_POS   = 0;
 	private static final int  CHILD_POS    = 1;
 	private static final int  FEATURE_POS  = 1;
-	private static final int  LABEL_POS    = 1;
-	private static final int  LEAF_POS     = 1;
-	private static final int  LEVEL_POS    = 1;
 	private static final int  SPLIT_POS    = 1;
-	private static final int  VALUE_POS	   = 1;
 	
 	private static final String INPUT_DIR  = System.getProperty("user.dir") + 
 										   "/demo/model_analysis";
@@ -140,7 +135,7 @@ public class ModelAnalyzer {
 		Map<String, String> split_point_map    = new HashMap<String, String>();
 		Map<String,List<String>> leaf_node_map = new HashMap<String, List<String>>();*/
 		Map<String,String> hashcode_label_mapping   = new HashMap<String, String>();
-		Map<String,String> edge_split_point_mapping = new HashMap<String, String>();
+		Map<String,List<String>> edge_split_point_mapping = new HashMap<String, List<String>>();
 		Map<String,List<String>> edge_map		    = new HashMap<String,List<String>>();
 		
 		try {
@@ -198,6 +193,9 @@ public class ModelAnalyzer {
 					 
 					 labelSplit     = label.split("\"");
 					 String split   = labelSplit[SPLIT_POS];
+					 split 			= split.replace("<", "");
+					 split 			= split.replace(">", "");
+					 split			= split.replace("=", "");
 					 
 					 if (edge_map.containsKey(parent)) {
 						 List<String> children = edge_map.get(parent);
@@ -208,15 +206,27 @@ public class ModelAnalyzer {
 						 children.add(child);
 						 edge_map.put(parent, children);	
 					 }
-					 edge_split_point_mapping.put(edge, split);
+					 
+					 if (edge_split_point_mapping.containsKey(parent)) {
+						 List<String> split_points = edge_split_point_mapping.get(parent);
+						 if (!split_points.contains(split))
+							 split_points.add(split);
+					 }
+					 else {
+						 List<String> split_points = new ArrayList<String>();
+						 split_points.add(split);
+						 edge_split_point_mapping.put(parent, split_points);	
+					 }
 				}
 				line = br.readLine();
 			}
+			/* DEBUG 
 			System.out.println(hashcode_label_mapping);
 			System.out.println(edge_map);
-			System.out.println(edge_split_point_mapping);
+			System.out.println(edge_split_point_mapping);*/
 			
 			JSONArray children 		  = null;
+			JSONArray root_splits	  = null;
 			JSONObject[] jsonObjArray = new JSONObject[hashcode_label_mapping.keySet().size()];
 			for (int i = 0; i < jsonObjArray.length; i++)
 				 jsonObjArray[i] = new JSONObject();
@@ -224,138 +234,57 @@ public class ModelAnalyzer {
 			int jsonObjArrayIndex		   = 1;
 			JSONObject rootJSONObj    	   = jsonObjArray[0];
 			List<String> rootChildren 	   = edge_map.get(rootHashCode);
+			List<String> childrenToProcess = new ArrayList<String>(rootChildren);
+
 			for (int i = 0; i < rootChildren.size(); i++) {
 				 rootChildren.set(i, hashcode_label_mapping.get(rootChildren.get(i)));
 			}
 			
- 			List<String> childrenToProcess = new ArrayList<String>(rootChildren);
-			
-			children = new JSONArray(rootChildren);
+			children 	= new JSONArray(rootChildren); 
+			root_splits = new JSONArray(edge_split_point_mapping.get(rootHashCode));
 			rootJSONObj.put("Feature", hashcode_label_mapping.get(rootHashCode));
-			rootJSONObj.put("Split Point", "null");
+			rootJSONObj.put("Split Point", root_splits);
 			rootJSONObj.put("Children", children);
 			
 			while(!childrenToProcess.isEmpty()) {
 				   JSONObject childJSONObj    = jsonObjArray[jsonObjArrayIndex++];
-				   String child_feature       = childrenToProcess.remove(0);
-				   List<String> childChildren = null;
-				   childrenToProcess.addAll(childChildren);
+				   String childHashCode 	  = childrenToProcess.remove(0);
+				   String childFeature        = hashcode_label_mapping.get(childHashCode);
+				   List<String> childChildren = edge_map.get(childHashCode);
+				   if (childChildren != null) {
+					   childrenToProcess.addAll(childChildren);
 				   
+					   for (int i = 0; i < childChildren.size(); i++) {
+						    childChildren.set(i, hashcode_label_mapping.get(childChildren.get(i)));
+					   }
+				   }
 				   JSONArray jsonChildChildren = new JSONArray(childChildren);
-				   childJSONObj.put("Feature", child_feature);
-				   childJSONObj.put("Split Point", "null");
+				   JSONArray child_splits 	   = new JSONArray(edge_split_point_mapping.get(childHashCode));
+				   childJSONObj.put("Feature", childFeature);
+				   childJSONObj.put("Split Point", child_splits);
 				   childJSONObj.put("Children", jsonChildChildren);
 			}
+			printJSONObjectArray(jsonObjArray);
+			
+			String filename = new SimpleDateFormat("yyyy-MM-dd_hh-mm-ss").format(new Date());
+			filename 	    = "3.0-random_tree-" + filename  + ".json";
+			filename		= OUTPUT_DIR + "/" + filename;
+			
+			PrintWriter writer = new PrintWriter(filename, "UTF-8");
+			for (JSONObject jsonObject: jsonObjArray){
+				 writer.println(jsonObject.toString());
+			}
+			writer.close();
+			
 		} catch (IOException | JSONException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 		}
-
 		
-				/*String[] line_split   = line.split(" ");
-				String[] branch_split = line_split[BRANCH_POS].split(":");
-				String branch_ID	  = branch_split[BRANCH_POS];
-				int level			  = Integer.parseInt(branch_ID.split("_")[LEVEL_POS]);
-				max_level 			  = (level > max_level) ? level : max_level;
-				String child 		  = branch_split[VALUE_POS];
-				String feature_String = "";
-				 
-				feature_String += child + " ";
-				for (int i = 1; i < line_split.length; i++) {
-					 feature_String += line_split[i] + " ";
-				}
-				List<String> features = new ArrayList<String>();
-				features.add(feature_String);
-				 
-				if (branch_map.containsKey(branch_ID)) {
-					List<String> feature_List = branch_map.get(branch_ID);
-					feature_List.addAll(features);
-				}
-				else {
-					branch_map.put(branch_ID, features);
-				}
-				line = br.readLine();
-			}
-			System.out.println(branch_map);
-
-			List<String> rootChildrenNodes	 = branch_map.get("Branch_0");
-			// ASSUMPTION: the root is not a leaf...
-			String id					     = rootChildrenNodes.get(0).split(" ")[NODE_ID_POS].split(":")[VALUE_POS];
-			String root_feature			     = id + "_" + rootChildrenNodes.get(0).split(" ")[FEATURE_POS].split(":")[VALUE_POS];
-			String root_split_point			 = rootChildrenNodes.get(0).split(" ")[SPLIT_POS].split(":")[VALUE_POS];
-			gen_map.put(root_feature, new ArrayList<String>());
-			split_point_map.put(root_feature, root_split_point);
-
-			for(int i = 1; i <= max_level; i++) {
-				String key = "Branch_";
-				key 	  += i;
-				List<String> rTreeNodes = branch_map.get(key);
-				for (int j = 0; j < rTreeNodes.size(); j++) {
-					 String child 		  = rTreeNodes.get(j++);
-					 String[] child_split = child.split(" - ");
-					 String child_id		  = child_split[0].split(" ")[NODE_ID_POS].split(":")[VALUE_POS];
-					 String child_feature  	  = child_id + "_" + child_split[0].split(" ")[FEATURE_POS].split(":")[VALUE_POS];
-					 String parent_feature 	  = child_split[0].split(" ")[PARENT_POS].split(":")[VALUE_POS];
-					 String child_split_point = child_split[0].split(" ")[SPLIT_POS].split(":")[VALUE_POS]; 
-						
-					 List<String> children 	 = gen_map.get(parent_feature);
-					 children.add(child_feature);
-					 // System.out.println("Children of " + parent_feature + ":" + children);
-					 split_point_map.put(child_feature, child_split_point);
-					 
-					 if(!gen_map.keySet().contains(child_feature)) {
-					    gen_map.put(child_feature, new ArrayList<String>());
-					 }
-					 
-					 if (child_split.length > 1) {// 'child' DOES contains a leaf
-						 String leaf  		   = child_split[LEAF_POS].replaceFirst(" ", "_");
-						 List<String> new_leaf = new ArrayList<String>();  
-						 
-						 if (leaf_node_map.containsKey(child_feature)) {
-							 List<String> leaf_list = leaf_node_map.get(child_feature);
-							 leaf_list.add(leaf);
-							}
-						 else {
-							 new_leaf.add(leaf);
-							 leaf_node_map.put(child_feature, new_leaf);
-						 }
-						 
-					 }
-				}
-			}
-			System.out.println(gen_map);
-			System.out.println(leaf_node_map);
-			JSONArray children 		  = null;
-			JSONObject[] jsonObjArray = new JSONObject[gen_map.keySet().size()];
-			for (int i = 0; i < jsonObjArray.length; i++)
-				 jsonObjArray[i] = new JSONObject();
-			
-			int jsonObjArrayIndex		   = 1;
-			JSONObject rootJSONObj    	   = jsonObjArray[0];
-			List<String> rootChildren 	   = gen_map.get(root_feature);
-			List<String> childrenToProcess = new ArrayList<String>(rootChildren);
-			
-			children = new JSONArray(rootChildren);
-			rootJSONObj.put("Feature", root_feature);
-			rootJSONObj.put("Split Point", root_split_point);
-			rootJSONObj.put("Children", children);
-			
-			while(!childrenToProcess.isEmpty()) {
-				   JSONObject childJSONObj    = jsonObjArray[jsonObjArrayIndex++];
-				   String child_feature       = childrenToProcess.remove(0);
-				   List<String> childChildren = gen_map.get(child_feature);
-				   childrenToProcess.addAll(childChildren);
-				   
-				   JSONArray jsonChildChildren = new JSONArray(childChildren);
-				   childJSONObj.put("Feature", child_feature);
-				   childJSONObj.put("Split Point", split_point_map.get(child_feature));
-				   childJSONObj.put("Children", jsonChildChildren);*/
-			
-			
 	}
 
 	private void printJSONObjectArray(JSONObject[] jsonObjArray) {
-		for (JSONObject jsonObject: jsonObjArray){
+		for (JSONObject jsonObject : jsonObjArray){
 			 System.out.println(jsonObject.toString());
 		}
 	}
